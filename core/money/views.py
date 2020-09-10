be46b4c7eea6046b9d711349b0642a9251efb189
aguterrestre <aguterrestre@gmail.com>
2020-09-10 19:56:06 -0300

    modulo-money-dashboard

--- core/money/views.py
+++ core/money/views.py
@@ -4,8 +4,12 @@ from django.urls import reverse_lazy
 from django.db.models import Sum
 from django.db.models.functions import Coalesce
 
+# from datetime import datetime
+import datetime
+
 from core.setting.models import Company
 from core.sale.models import Ticket
+from core.purchase.models import Invoice
 
 
 class MoneyDashboardView(LoginRequiredMixin, TemplateView):
@@ -14,6 +18,47 @@ class MoneyDashboardView(LoginRequiredMixin, TemplateView):
     """
 
     template_name = 'dashboard_money.html'
+
+    def get_card_money_percentage_increase(self, obj):
+        increase = 0
+        try:
+            # obtengo la fecha actual
+            current_date = datetime.date.today()
+            # obtengo datos numericos del ultimo día del mes pasado
+            last_month = current_date.replace(day=1)+datetime.timedelta(days=-1)
+            # inicio variable del primer dia del mes anterior
+            start_date = datetime.date(last_month.year, last_month.month, 1)
+            # inicio variable del ultimo dia del mes anterior
+            end_date = datetime.date(last_month.year, last_month.month, last_month.day)
+            # obtengo todas las ventas del mes pasado
+            total_last_month = obj.objects.filter(date_creation__range=(start_date, end_date))
+            # sumo los totales del mes pasado
+            total_last_month_tot = total_last_month.aggregate(r=Coalesce(Sum('total'), 0)).get('r')
+            # saco promedio de venta del mes pasado
+            total_last_month_tot = total_last_month_tot / last_month.day
+            # obtengo datos numericos del ultimo dia del mes actual
+            cur_month = current_date.replace(month=current_date.month+1, day=1) - datetime.timedelta(days=1)
+            # inicio variable del primer dia del mes actual
+            start_date = datetime.date(cur_month.year, cur_month.month, 1)
+            # inicio variable del ultimo dia del mes actual
+            end_date = datetime.date(cur_month.year, cur_month.month, cur_month.day)
+            # obtengo todas las ventas del mes actual
+            total_current_month = obj.objects.filter(date_creation__range=(start_date, end_date))
+            # sumo los totales del mes actual
+            total_current_month_tot = total_current_month.aggregate(r=Coalesce(Sum('total'), 0)).get('r')
+            # saco promedio de venta del mes actual
+            total_current_month_tot = total_current_month_tot / cur_month.day
+            # calculo de aumento de ventas del mes actual sin decimales
+            increase = int((((total_current_month_tot / total_last_month_tot) - 1) * 100))
+            # controlamos cuando el aumento es negativo ya el total del mes pasado es mayor al actual
+            if increase < 0:
+                increase = 0
+            # esta faltando ajustar el calculo de ultimo dia del mes cuando hay cambio de año
+        except ZeroDivisionError:
+            increase = 100  # al no tener totales del mes pasado mostramos un 100%
+        except Exception:
+            increase = 0
+        return increase
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,5 +70,15 @@ class MoneyDashboardView(LoginRequiredMixin, TemplateView):
         context['dashboard_url'] = reverse_lazy('login:dashboard')
         # Card de ingresos totales
         ticket_total = Ticket.objects.all()
-        context['card_money_ticket_num_total'] = ticket_total.aggregate(r=Coalesce(Sum('total'), 0)).get('r')
+        ticket_num_total = ticket_total.aggregate(r=Coalesce(Sum('total'), 0)).get('r')
+        context['card_money_ticket_num_total'] = ticket_num_total
+        context['card_money_ticket_sale_increase'] = self.get_card_money_percentage_increase(Ticket)
+        # Card de egresos totales
+        invo_total = Invoice.objects.all()
+        invoice_num_total = invo_total.aggregate(r=Coalesce(Sum('total'), 0)).get('r')
+        context['card_money_invoice_num_total'] = invoice_num_total
+        context['card_money_invoice_purchase_increase'] = self.get_card_money_percentage_increase(Invoice)
+        # Card de resultados totales
+        total_result = ticket_num_total - invoice_num_total
+        context['card_money_total_result'] = total_result
         return context
